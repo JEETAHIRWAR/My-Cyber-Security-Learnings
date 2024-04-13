37d4cf7592f53e9f17f5f7eacc1b2489757a2f7c
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-14 01:08:51 +0530

    more

--- script.js
+++ script.js
@@ -9,6 +9,47 @@ document.addEventListener("DOMContentLoaded", function () {
         const logo = document.querySelector(".logo span");
         const card = document.querySelectorAll(".card");
         const title = document.querySelectorAll(".title h3");
+        const para = document.querySelectorAll(".para");
+        const ul = document.querySelectorAll(".list ul li");
+        const h4 = document.querySelectorAll("h4");
+        const aCard = document.querySelectorAll(".aCard");
+        const h2 = document.querySelectorAll("h2");
+        const smallPara = document.querySelectorAll(".smallPara");
+        const faRoad = document.querySelectorAll(".fa-road");
+
+
+        faRoad.forEach(faRoad => {
+            faRoad.classList.toggle("dark");
+        })
+
+
+        smallPara.forEach(smallPara => {
+            smallPara.classList.toggle("dark");
+        })
+
+
+        h2.forEach(h2 => {
+            h2.classList.toggle("dark");
+        })
+
+
+        aCard.forEach(aCard => {
+            aCard.classList.toggle("dark");
+        })
+
+
+        h4.forEach(h4 => {
+            h4.classList.toggle("dark");
+        })
+
+        ul.forEach(ul => {
+            ul.classList.toggle("dark");
+        })
+
+
+        para.forEach(para => {
+            para.classList.toggle("dark");
+        })
 
 
         title.forEach(title => {
@@ -99,6 +140,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+    const buttons = document.querySelectorAll('.button[data-show]');
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const target = button.getAttribute('data-show');
+            // const target = this.getAttribute('data-show');
+            const hide = this.getAttribute('data-hide');
+            const container = document.querySelector(hide);
+            fetch(`/${target}`)
+                .then(response => response.text())
+                .then(data => {
+                    container.innerHTML = data;
+                    container.style.display = 'block';
+                })
+                .catch(error => console.error('Error fetching content:', error));
+        });
+    });
+
+
+
+
+
+
 
     const cards = document.querySelectorAll('.card');
 
@@ -126,28 +189,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
-
-// const checkbox = document.getElementById("checkbox")
-// checkbox.addEventListener("change", () => {
-//     const mainSection = document.querySelector(".mainSection");
-//     const listgridView = document.querySelector(".listgridView");
-//     const checkbox = document.getElementById("checkbox");
-//     const search = document.querySelector(".search-input");
-//     const logo = document.querySelector(".logo span");
-//     const card = document.querySelectorAll(".card");
-
-
-//     logo.classList.toggle("dark");
-//     mainSection.classList.toggle("dark");
-//     listgridView.classList.toggle("dark");
-//     checkbox.classList.toggle("dark");
-//     search.classList.toggle("dark");
-//     document.body.classList.toggle("dark");
-
-//     for (let i = 0; i < card.length; i++) {
-//         card[i].classList.toggle("dark");
-//     }
-
-// })
